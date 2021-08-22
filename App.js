5a63ffe0e50a761f10974d246939c0e620c7f1db
kanghadong <hdkang2007@gmail.com>
2021-08-22 17:16:54 +0900

    tesla clone rn app

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Header from './components/Header';
+import CarsList from './components/CarsList';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header />
+      <CarsList />
     </View>
   );
 }
